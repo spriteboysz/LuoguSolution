6285253cf99e469ccbcc0ef7672d81b13a4a67a4
spriteboysz <sprite_bx@126.com>
2022-01-01 23:40:31 +0800

    20220101_03

--- Main/P1789.py
+++ Main/P1789.py
@@ -3,31 +3,39 @@
 '''
 Author: Deean
 Date: 2021-12-05 23:20:34
-LastEditTime: 2021-12-05 23:41:01
+LastEditTime: 2022-01-01 23:39:02
 Description: 【Mc生存】插火把
 FilePath: P1789.py
 '''
 
 
-def func(): 
+def func():
+    torch = [[-2, 0], [2, 0], [0, 2], [0, -2],
+             [-1, -1], [-1, 0], [-1, 1],
+             [0, -1], [0, 0], [0, 1],
+             [1, -1], [1, 0], [1, 1]]
+    fluorite = [[-2, -2], [-2, -1], [-2, 0], [-2, 1], [-2, 2],
+                [-1, -2], [-1, -1], [-1, 0], [-1, 1], [-1, 2],
+                [0, -2], [0, -1], [0, 0], [0, 1], [0, 2],
+                [1, -2], [1, -1], [1, 0], [1, 1], [1, 2],
+                [2, -2], [2, -1], [2, 0], [2, 1], [2, 2]]
     n, m, k = map(int, input().strip().split())
-    matrix = [[1] * n for _ in range(n)]
-    torch, fluorite = [], []
+    matrix = [[1] * (n + 4) for _ in range(n + 4)]
     for _ in range(m):
-        x, y = map(int, input().strip().split())
-        for i in range(max(0, x - 1 - 2), min(n - 1, x - 1 + 2) + 1):
-            for j in range(max(0, y - 1 - 2), min(n - 1, y - 1 + 2) + 1):
-                pass
+        x, y = map(lambda el: int(el) - 1 + 2, input().strip().split())
+        for item in torch:
+            matrix[x + item[0]][y + item[1]] = 0
 
-        
     for _ in range(k):
-        x, y = map(int, input().strip().split())
-        for i in range(max(0, x - 1 - 2), min(n - 1, x - 1 + 2) + 1):
-            for j in range(max(0, y - 1 - 2), min(n - 1, y - 1 + 2) + 1):
-                matrix[i][j] = 0
-    
-    print(matrix)
+        x, y = map(lambda el: int(el) - 1 + 2, input().strip().split())
+        for item in fluorite:
+            matrix[x + item[0]][y + item[1]] = 0
+
+    count = 0
+    for row in range(2, 2 + n):
+        count += matrix[row][2:2 + n].count(1)
+    print(count)
+
 
 if __name__ == '__main__':
     func()
-    
