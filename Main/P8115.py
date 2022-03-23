0615cb0b4e51d053e15abe689b0680c0e55b13dd
deean <sprit_bx@126.com>
2022-03-23 23:19:13 +0800

    2022032323

---
+++ Main/P8115.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# coding=utf-8
+"""
+Author: Deean
+Date: 2022-03-23 22:37:57
+LastEditTime: 2022-03-23 23:01:04
+Description: 
+FilePath: P8115.py
+"""
+
+
+def func():
+    nums = input().lstrip("{").rstrip("}").split(",")
+    mixed = []
+    for num in filter(lambda el: el != "", nums):
+        hexadecimal = hex(int(num)).upper().replace("X", "x")
+        if len(hexadecimal) <= len(num):
+            mixed.append(hexadecimal)
+        else:
+            mixed.append(num)
+    print("{" + ",".join(mixed) + "}")
+
+
+if __name__ == "__main__":
+    func()
